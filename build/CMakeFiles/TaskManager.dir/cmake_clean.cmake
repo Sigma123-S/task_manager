8178ac08dc8fac3c65f244a3cc474310405bdbac
Da <awpawm321@gmail.com>
2026-03-09 23:31:57 +0300

    Исправление структуры проекта и добавление сборки Cmake

---
+++ build/CMakeFiles/TaskManager.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TaskManager.dir/link.d"
+  "CMakeFiles/TaskManager.dir/src/Task.cpp.o"
+  "CMakeFiles/TaskManager.dir/src/Task.cpp.o.d"
+  "CMakeFiles/TaskManager.dir/src/TaskManager.cpp.o"
+  "CMakeFiles/TaskManager.dir/src/TaskManager.cpp.o.d"
+  "CMakeFiles/TaskManager.dir/src/main.cpp.o"
+  "CMakeFiles/TaskManager.dir/src/main.cpp.o.d"
+  "TaskManager"
+  "TaskManager.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TaskManager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
